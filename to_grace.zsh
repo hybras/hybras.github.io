45d1b2f455719b5731676b344950d912b6fd818f
hybras <24651269+hybras@users.noreply.github.com>
2020-10-31 12:52:45 -0400

    Use env var for sshpass in deploy script

--- to_grace.zsh
+++ to_grace.zsh
@@ -1,4 +1,4 @@
 jekyll build
 find _site -type f -exec sed -i 's:href="/:href="/~vchari/:g' {} \;
 find _site -type f -exec sed -i 's:src="/:src="/~vchari/:g' {} \;
-sshpass -p "$SSHPASS" scp -r _site/* vchari@grace.umd.edu:/pub/vchari
+sshpass -e scp -r _site/* vchari@grace.umd.edu:/pub/vchari
