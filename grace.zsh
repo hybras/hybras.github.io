58067336a8ec0c6b44a53a4b8d8858572b251046
hybras <24651269+hybras@users.noreply.github.com>
2020-11-30 18:29:26 -0500

    Modify avatar url as well

--- grace.zsh
+++ grace.zsh
@@ -1,10 +1,12 @@
 # edit url, baseurl in _config.yml
 sd "url: https://hybras.github.io" "url: https://www.terpconnect.umd.edu" _config.yml
 sd "baseurl: \"\"" "baseurl: \"/~vchari\"" _config.yml
+sd "avatar: /images/profile.jpg" "avatar: /~vchari/images/profile.jpg" _config.yml
 
 bundle exec jekyll build --incremental
 
 sd "url: https://www.terpconnect.umd.edu" "url: https://hybras.github.io" _config.yml
 sd "baseurl: \"/~vchari\"" "baseurl: \"\"" _config.yml
+sd "avatar: /~vchari/images/profile.jpg" "avatar: /images/profile.jpg" _config.yml
 
 sshpass -f ~/.umd scp -r _site  umdb:~/../pub
