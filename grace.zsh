f76660ee158baeb8e3f26a5f2cf80d7e46be04fd
hybras <24651269+hybras@users.noreply.github.com>
2020-11-30 16:58:58 -0500

    Beef up to grace script

--- grace.zsh
+++ grace.zsh
@@ -1,1 +1,10 @@
-jekyll build --incremental --baseurl "\~vchari"
+# edit url, baseurl in _config.yml
+sd "url: https://hybras.github.io" "url: https://www.terpconnect.umd.edu" _config.yml
+sd "baseurl: \"\"" "baseurl: \"/~vchari\"" _config.yml
+
+jekyll build
+
+sd "url: https://www.terpconnect.umd.edu" "url: https://hybras.github.io" _config.yml
+sd "baseurl: \"/~vchari\"" "baseurl: \"\"" _config.yml
+
+sshpass -f ~/.umd scp -r _site  umdb:~/../pub
