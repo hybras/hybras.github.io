19e8c6e08db9e75763b5e90581ed67cf588d7b78
hybras <24651269+hybras@users.noreply.github.com>
2023-06-12 19:22:08 -0400

    Use flake utils

--- flake.nix
+++ flake.nix
@@ -2,27 +2,19 @@
   description = "A Nix Environment for my website";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs allSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.hugo
-            pkgs.asciidoctor-with-extensions
-          ];
-        };
-      });
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = [
+                pkgs.hugo
+                pkgs.asciidoctor-with-extensions
+              ];
+            };
+          };
+        });
 }
